acc7bd08bd497dffcb1e909aaaf535c1208d8dae
wanglin1201 <354252301@qq.com>
2018-03-08 10:59:45 +0800

    add

---
+++ demo01.py
@@ -0,0 +1,13 @@
+import sendmsg
+# import revdmsg
+
+
+#from sendmsg import *   #__all__  from引入模块
+from sendmsg import *   #__all__  from引入模块
+
+# sendmsg.send()
+
+# sendmsg.test()
+
+zs = sendmsg.Person()
+zs.eat()
